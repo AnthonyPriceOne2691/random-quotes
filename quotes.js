77a585466f01946f32a3f20a27e68e9727bbafba
AnthonyPriceOne2691 <anthonypriceone@gmail.com>
2024-08-21 00:13:34 +0300

    Show favorite quotes

--- quotes.js
+++ quotes.js
@@ -3,48 +3,58 @@ const quotes = [
     quote:
       'The only limit to our realization of tomorrow is our doubts of today.',
     author: 'Franklin D. Roosevelt',
+    isFavorite: false,
   },
   {
     quote:
       'In the end, we will remember not the words of our enemies, but the silence of our friends.',
     author: 'Martin Luther King Jr.',
+    isFavorite: false,
   },
   {
     quote:
       'To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.',
     author: 'Ralph Waldo Emerson',
+    isFavorite: false,
   },
   {
     quote:
       'Success is not final, failure is not fatal: It is the courage to continue that counts.',
     author: 'Winston Churchill',
+    isFavorite: false,
   },
   {
     quote:
       'Imagination is more important than knowledge. For knowledge is limited, whereas imagination embraces the entire world.',
     author: 'Albert Einstein',
+    isFavorite: false,
   },
   {
     quote: 'The best way to predict your future is to create it.',
     author: 'Peter Drucker',
+    isFavorite: false,
   },
   {
     quote: "Life is what happens when you're busy making other plans.",
     author: 'John Lennon',
+    isFavorite: false,
   },
   {
     quote:
       'Do not wait to strike till the iron is hot, but make it hot by striking.',
     author: 'William Butler Yeats',
+    isFavorite: false,
   },
   {
     quote: 'It does not matter how slowly you go as long as you do not stop.',
     author: 'Confucius',
+    isFavorite: false,
   },
   {
     quote:
       'Our greatest glory is not in never falling, but in rising every time we fall.',
     author: 'Confucius',
+    isFavorite: false,
   },
 ];
 
